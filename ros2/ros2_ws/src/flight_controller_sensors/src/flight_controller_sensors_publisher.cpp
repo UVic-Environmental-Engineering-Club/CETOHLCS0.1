28f35b321caa67c86e1ec085d6f1dbaa7fcc8ed2
Sebastien Robitaille <sebastien@robitaille.info>
2026-08-22 15:49:43 -0700

    Add basic publisher implemetation for imu and magnetic field data from fligh controller sensors

--- ros2/ros2_ws/src/flight_controller_sensors/src/flight_controller_sensors_publisher.cpp
+++ ros2/ros2_ws/src/flight_controller_sensors/src/flight_controller_sensors_publisher.cpp
@@ -1,6 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/string.hpp"
 
+#include "sensor_msgs/msg/imu.hpp"
+#include "sensor_msgs/msg/magnetic_field.hpp"
+
 #include "bindings.h"
 #include <stdint.h>
 #include <stdio.h>
@@ -13,26 +16,29 @@ class FlightControllerSensorsPublisher : public rclcpp::Node
     public:
         FlightControllerSensorsPublisher() : Node("flight_controller_sensors_publisher")
         {
-            publisher_ = this->create_publisher<example_interfaces::msg::String>("string_test_topic", 10);
 
-            
+            get_parameter_or<std::string>("frame_id", _frame_id, "imu");
+            get_parameter_or<int>("poll_rate", _poll_rate, 1000);
+
+            get_parameter_or<std::string>("topicImu", _topicImu, "/imu/data_raw");
+            get_parameter_or<std::string>("topicMag", _topicMag, "/imu/mag");
+
+            _publisherIMU = this->create_publisher<sensor_msgs::msg::Imu>(_topicImu, 10);
+            _publisherMag = this->create_publisher<sensor_msgs::msg::MagneticField>(_topicMag, 10);
+
             timer_ = this->create_wall_timer(
-                std::chrono::milliseconds(1000),
+                std::chrono::milliseconds(_poll_rate),
                 std::bind(&FlightControllerSensorsPublisher::publish_sensor_data, this));
-            
-            _couter = 0;
 
             RCLCPP_INFO(this->get_logger(), "Initiating navigator module.\n");
             init();
 
-            RCLCPP_INFO(this->get_logger(), "Setting led on!\n");
-            set_led(UserLed::Led1, true);
+            if(!self_test()){
+                RCLCPP_ERROR(this->get_logger(), "Navigator self test failed. Exiting.\n");
+                rclcpp::shutdown();
+            }
 
-            RCLCPP_INFO(this->get_logger(), "Temperature: %f\n", read_temp());
-            RCLCPP_INFO(this->get_logger(), "Pressure: %f\n", read_pressure());
-
-            AxisData mag = read_mag();
-            RCLCPP_INFO(this->get_logger(), "Magnetic field: X = %f, Y = %f, Z = %f\n", mag.x, mag.y, mag.z);
+            RCLCPP_INFO(this->get_logger(), "Navigator self test passed.\n");
 
         }
     
@@ -40,16 +46,51 @@ class FlightControllerSensorsPublisher : public rclcpp::Node
 
         void publish_sensor_data()
         {
-            auto message = example_interfaces::msg::String();
-            message.data = "Hello, this is a test message from the flight controller sensors publisher with lib import! Count: " + std::to_string(_couter);
-            RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-            publisher_->publish(message);
-            _couter++;
+            AxisData mag = read_mag();
+            AxisData accel = read_accel();
+            AxisData gyro = read_gyro();
+
+            auto mag_msg = sensor_msgs::msg::MagneticField();
+            mag_msg.header.frame_id = _frame_id;
+            mag_msg.header.stamp = this->now();
+
+            mag_msg.magnetic_field.x = mag.x/1000000.0; // Convert from microtesla to tesla
+            mag_msg.magnetic_field.y = mag.y/1000000.0; 
+            mag_msg.magnetic_field.z = mag.z/1000000.0; 
+
+            _publisherMag->publish(mag_msg);
+            RCLCPP_INFO(this->get_logger(), "Published magnetic field data: x=%f, y=%f, z=%f", mag.x, mag.y, mag.z);
+
+
+            auto imu_msg = sensor_msgs::msg::Imu();
+            imu_msg.header.frame_id = _frame_id;
+            imu_msg.header.stamp = mag_msg.header.stamp;
+
+            imu_msg.linear_acceleration.x = accel.x;
+            imu_msg.linear_acceleration.y = accel.y;
+            imu_msg.linear_acceleration.z = accel.z;
+
+            imu_msg.angular_velocity.x = gyro.x;
+            imu_msg.angular_velocity.y = gyro.y;
+            imu_msg.angular_velocity.z = gyro.z;
+
+            _publisherIMU->publish(imu_msg);
+            RCLCPP_INFO(this->get_logger(), "Published IMU data: accel x=%f, y=%f, z=%f; gyro x=%f, y=%f, z=%f", 
+                        accel.x, accel.y, accel.z, gyro.x, gyro.y, gyro.z);
+
         }    
 
-        rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher_;
+        rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr _publisherIMU;
+        rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr _publisherMag;
         rclcpp::TimerBase::SharedPtr timer_;
-        size_t _couter;
+
+        int _poll_rate;
+
+        std::string _frame_id;
+
+        std::string _topicImu;
+        std::string _topicMag;
+
 };
 
 int main(int argc, char * argv[])
@@ -57,6 +98,14 @@ int main(int argc, char * argv[])
 
     rclcpp::init(argc, argv);
     auto node = std::make_shared<FlightControllerSensorsPublisher>();
+
+    node->declare_parameter<std::string>("frame_id", "imu");
+    node->declare_parameter<int>("poll_rate", 1000);
+
+    node->declare_parameter<std::string>("topicImu", "/imu/data_raw");
+    node->declare_parameter<std::string>("topicMag", "/imu/mag");
+    
+
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
