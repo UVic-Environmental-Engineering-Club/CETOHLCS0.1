7cc0e1af5dc0d3846d576f731614948db977e462
Sebastien Robitaille <sebastien@robitaille.info>
2026-08-22 14:42:12 -0700

    Setup blue robotics cpp library in flight controller test node

--- ros2/ros2_ws/src/flight_controller_sensors/src/flight_controller_sensors_publisher.cpp
+++ ros2/ros2_ws/src/flight_controller_sensors/src/flight_controller_sensors_publisher.cpp
@@ -1,6 +1,12 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/string.hpp"
 
+#include "bindings.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 
 class FlightControllerSensorsPublisher : public rclcpp::Node
 {
@@ -16,6 +22,18 @@ class FlightControllerSensorsPublisher : public rclcpp::Node
             
             _couter = 0;
 
+            RCLCPP_INFO(this->get_logger(), "Initiating navigator module.\n");
+            init();
+
+            RCLCPP_INFO(this->get_logger(), "Setting led on!\n");
+            set_led(UserLed::Led1, true);
+
+            RCLCPP_INFO(this->get_logger(), "Temperature: %f\n", read_temp());
+            RCLCPP_INFO(this->get_logger(), "Pressure: %f\n", read_pressure());
+
+            AxisData mag = read_mag();
+            RCLCPP_INFO(this->get_logger(), "Magnetic field: X = %f, Y = %f, Z = %f\n", mag.x, mag.y, mag.z);
+
         }
     
     private:
@@ -23,7 +41,7 @@ class FlightControllerSensorsPublisher : public rclcpp::Node
         void publish_sensor_data()
         {
             auto message = example_interfaces::msg::String();
-            message.data = "Hello, this is a test message from the flight controller sensors publisher! Count: " + std::to_string(_couter);
+            message.data = "Hello, this is a test message from the flight controller sensors publisher with lib import! Count: " + std::to_string(_couter);
             RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
             publisher_->publish(message);
             _couter++;
@@ -36,6 +54,7 @@ class FlightControllerSensorsPublisher : public rclcpp::Node
 
 int main(int argc, char * argv[])
 {
+
     rclcpp::init(argc, argv);
     auto node = std::make_shared<FlightControllerSensorsPublisher>();
     rclcpp::spin(node);
