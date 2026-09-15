666f7d2d94e7880a6b39f38a49b9973696d07227
Sebastien Robitaille <sebastien@robitaille.info>
2026-09-14 22:00:49 -0700

    Add basic llcs watchdog monitoring

--- ros2/ros2_ws/src/safety_watchdog/src/safety_watchdog.cpp
+++ ros2/ros2_ws/src/safety_watchdog/src/safety_watchdog.cpp
@@ -3,6 +3,9 @@
 #include "ceto_interfaces/msg/hlcs_heartbeat.hpp"
 #include "ceto_interfaces/msg/llcs_heartbeat.hpp"
 
+#define HEARTBEAT_PUBLISH_INTERVAL_MS 100
+#define HEARTBEAT_TIMEOUT_MS 1000
+
 class SafetyWatchdog : public rclcpp::Node
 {
     public:
@@ -10,14 +13,28 @@ class SafetyWatchdog : public rclcpp::Node
         {
             _node_startup_time = this->now().nanoseconds() / 1000000; // Get the current time in milliseconds
 
+            // Initialize the last heartbeat time to the current time at startup giving us a one timeout period at startup.
+            _last_llcs_heartbeat_time = this->now().nanoseconds() / 1000000;
+
             _publisherHLCSHeartbeat = this->create_publisher<ceto_interfaces::msg::HLCSHeartbeat>("/heartbeat/hlcs", 10);
 
+            _subscriptionLLCSHeartbeat = this->create_subscription<ceto_interfaces::msg::LLCSHeartbeat>(
+                "/heartbeat/llcs",
+                10,
+                std::bind(&SafetyWatchdog::handle_llcs_heartbeat, this, std::placeholders::_1)
+            );
+
             _hlcs_heartbeat_timer = this->create_wall_timer(
-                std::chrono::milliseconds(100),
+                std::chrono::milliseconds(HEARTBEAT_PUBLISH_INTERVAL_MS),
                 std::bind(&SafetyWatchdog::publish_hlcs_heartbeat, this));
+
+            _llcs_watchdog_timer = this->create_wall_timer(
+                std::chrono::milliseconds(HEARTBEAT_PUBLISH_INTERVAL_MS),
+                std::bind(&SafetyWatchdog::check_llcs_heartbeat, this));
         }
     
     private:
+
         void publish_hlcs_heartbeat()
         {
             auto heartbeat_msg = ceto_interfaces::msg::HLCSHeartbeat();
@@ -29,14 +46,32 @@ class SafetyWatchdog : public rclcpp::Node
 
             _publisherHLCSHeartbeat->publish(heartbeat_msg);
 
+        }
 
+        void handle_llcs_heartbeat(const ceto_interfaces::msg::LLCSHeartbeat::SharedPtr msg)
+        {
+            // Handle the received LLCS heartbeat message
+            RCLCPP_INFO(this->get_logger(), "Received LLCS Heartbeat: timestamp=%ld, status=%d", msg->timestamp, msg->status);
+            _last_llcs_heartbeat_time = this->now().nanoseconds() / 1000000; // Update the last received heartbeat time in milliseconds
+
+        }
+
+        void check_llcs_heartbeat()
+        {
+            if (((this->now().nanoseconds() / 1000000) - _last_llcs_heartbeat_time) > HEARTBEAT_TIMEOUT_MS)
+            {
+                RCLCPP_ERROR(this->get_logger(), "LLCS heartbeat timeout! No heartbeat received for %d ms.", HEARTBEAT_TIMEOUT_MS);
+                // Here you can add additional actions to take when the LLCS heartbeat is lost, such as triggering a safety protocol.
+            }
         }
 
         rclcpp::Publisher<ceto_interfaces::msg::HLCSHeartbeat>::SharedPtr _publisherHLCSHeartbeat;
         rclcpp::TimerBase::SharedPtr _hlcs_heartbeat_timer;
+        rclcpp::TimerBase::SharedPtr _llcs_watchdog_timer;
+        rclcpp::Subscription<ceto_interfaces::msg::LLCSHeartbeat>::SharedPtr _subscriptionLLCSHeartbeat;
 
         long long _node_startup_time;
-
+        long long _last_llcs_heartbeat_time;
 };
 
 int main(int argc, char * argv[])
